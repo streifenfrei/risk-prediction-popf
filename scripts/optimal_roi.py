9913c2694fdb022fb5316f197139d999ec113d02
Martin Lelis <martinlelis@yahoo.de>
2021-04-16 12:07:19 +0200

    add more prints to optimal_roi.py script

--- scripts/optimal_roi.py
+++ scripts/optimal_roi.py
@@ -12,10 +12,15 @@ def main():
     min_roi_size = [-np.inf, -np.inf, -np.inf]
     count = 0
     for root, _, files in os.walk(args.data):
+        try:
+            int(os.path.basename(root))
+        except ValueError:
+            continue
         nrrd_files = set([x for x in files if x[-4:] == "nrrd"])
         segmentation_file = set([x for x in nrrd_files if x[-8:] == "seg.nrrd"])
         data_file = nrrd_files - segmentation_file
         if len(segmentation_file) != 1 or len(data_file) != 1:
+            print(f"Invalid data directory {root}. Ignoring")
             continue
         data_file = os.path.join(root, list(data_file)[0])
         segmentation_file = os.path.join(root, list(segmentation_file)[0])
