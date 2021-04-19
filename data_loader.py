db043b91cf8cc97a088078b465532244920d2dad
Martin Lelis <martinlelis@yahoo.de>
2021-04-19 21:56:19 +0200

    add cropping to binary and full segmentation in data loader

--- data_loader.py
+++ data_loader.py
@@ -64,14 +64,13 @@ class DataLoader(SlimDataLoaderBase, ABC):
             for patient_id, data_sitk, segmentation_sitk in batch_sitk:
                 if self.crop == "none":
                     data_np = sitk.GetArrayFromImage(data_sitk)
-                    data_np = np.expand_dims(np.transpose(data_np), axis=0)
+                    data_np = np.expand_dims(data_np.transpose(), axis=0)
                 else:
                     data_sitk_cropped = data_sitk[
-                                                     self.roi_root[0]:self.roi_root[0] + self.roi_size[0],
-                                                     self.roi_root[1]:self.roi_root[1] + self.roi_size[1],
-                                                     self.roi_root[2]:self.roi_root[2] + self.roi_size[2]]
-                    data_np = sitk.GetArrayFromImage(data_sitk_cropped)
-                    data_np = data_np.transpose()
+                                        self.roi_root[0]:self.roi_root[0] + self.roi_size[0],
+                                        self.roi_root[1]:self.roi_root[1] + self.roi_size[1],
+                                        self.roi_root[2]:self.roi_root[2] + self.roi_size[2]]
+                    data_np = sitk.GetArrayFromImage(data_sitk_cropped).transpose()
                     if self.crop in ["roi", "seg_bin", "seg"]:
                         segmentation_root = segmentation_sitk.TransformIndexToPhysicalPoint((0, 0, 0))
                         segmentation_root = data_sitk.TransformPhysicalPointToIndex(segmentation_root)
@@ -81,18 +80,24 @@ class DataLoader(SlimDataLoaderBase, ABC):
                         assert all(seg_root + seg_size <= roi_size for seg_root, seg_size, roi_size in
                                    zip(segmentation_root, segmentation_size, self.roi_size))
                         if self.crop == "roi":
-                            mask = np.zeros_like(data_np, dtype=np.bool)
+                            mask = np.ones_like(data_np, dtype=np.bool)
                             mask[segmentation_root[0]:segmentation_root[0] + segmentation_size[0],
                                  segmentation_root[1]:segmentation_root[1] + segmentation_size[1],
-                                 segmentation_root[2]:segmentation_root[2] + segmentation_size[2]] = True
-                            data_np = np.where(mask, data_np, 0)
+                                 segmentation_root[2]:segmentation_root[2] + segmentation_size[2]] = False
+                            data_np = np.expand_dims(np.where(mask, 0, data_np), axis=0)
                         else:
-                            segmentation_np = sitk.GetArrayFromImage(segmentation_sitk)
-                            # TODO crop to segmentation
-                        data_np = np.expand_dims(data_np, axis=0)
-
+                            segmentation_np = sitk.GetArrayFromImage(segmentation_sitk).transpose()
+                            if self.crop == "seg_bin":
+                                segmentation_np = segmentation_np.sum(axis=0, keepdims=True)
+                            data_layer = []
+                            for mask in np.split(segmentation_np, indices_or_sections=segmentation_np.shape[0], axis=0):
+                                padding = list((seg_root, roi_size - seg_size - seg_root)
+                                               for seg_root, seg_size, roi_size in
+                                               zip(segmentation_root, segmentation_size, data_np.shape))
+                                mask = np.pad(mask.squeeze(), padding, constant_values=False)
+                                data_layer.append(np.where(mask, data_np, 0))
+                            data_np = np.stack(data_layer)
                 batch_np.append(data_np)
-
             batch = {"data": np.stack(batch_np)}
 
             self.current_position += self.number_of_threads_in_multithreaded * self.batch_size
