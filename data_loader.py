6838fbd3aa1e597e85e3e5001bbb5893fe8a05cb
Martin Lelis <martinlelis@yahoo.de>
2021-04-19 19:22:23 +0200

    add cropping to fixed bb and roi in data loader

--- data_loader.py
+++ data_loader.py
@@ -7,10 +7,20 @@ import numpy as np
 
 from batchgenerators.dataloading import SlimDataLoaderBase
 
+
 class DataLoader(SlimDataLoaderBase, ABC):
-    def __init__(self, data_directory, batch_size, crop="none", number_of_threads_in_multithreaded=4):
+    def __init__(self,
+                 data_directory,
+                 batch_size,
+                 crop="none",
+                 number_of_threads_in_multithreaded=4,
+                 roi=None):
         super().__init__(None, batch_size, number_of_threads_in_multithreaded)
-        assert crop in ["none", "roi", "seg"]
+        assert crop in ["none", "fixed", "roi", "seg_bin", "seg"]
+        if crop != "none":
+            assert roi is not None
+            self.roi_root = roi[:3]
+            self.roi_size = roi[3:]
         self.crop = crop
         self._data = []
         for root, _, files in os.walk(data_directory):
@@ -38,6 +48,7 @@ class DataLoader(SlimDataLoaderBase, ABC):
         if not self.was_initialized:
             self.reset()
         if self.current_position < len(self._data):
+            # load files as sitk objects
             batch_sitk = []
             for i in range(self.batch_size):
                 index = self.current_position + i
@@ -46,15 +57,46 @@ class DataLoader(SlimDataLoaderBase, ABC):
                     data_sitk = sitk.ReadImage(data_file, imageIO="NrrdImageIO")
                     segmentation_sitk = None
                     if self.crop != "none":
-                        segmentation_sitk = sitk.ReadImage(data_file, imageIO="NrrdImageIO")
+                        segmentation_sitk = sitk.ReadImage(segmentation_file, imageIO="NrrdImageIO")
                     batch_sitk.append((patient_id, data_sitk, segmentation_sitk))
-
+            # preprocess data
             batch_np = []
-            if self.crop == "none":
-                for patient_id, data_sitk, _ in batch_sitk:
+            for patient_id, data_sitk, segmentation_sitk in batch_sitk:
+                if self.crop == "none":
                     data_np = sitk.GetArrayFromImage(data_sitk)
-                    data_np = np.expand_dims(np.moveaxis(data_np, 0, 2), axis=0)
-                    batch_np.append(data_np)
+                    data_np = np.expand_dims(np.transpose(data_np), axis=0)
+                else:
+                    data_sitk_cropped = data_sitk[
+                                                     self.roi_root[0]:self.roi_root[0] + self.roi_size[0],
+                                                     self.roi_root[1]:self.roi_root[1] + self.roi_size[1],
+                                                     self.roi_root[2]:self.roi_root[2] + self.roi_size[2]]
+                    data_np = sitk.GetArrayFromImage(data_sitk_cropped)
+                    data_np = data_np.transpose()
+                    if self.crop in ["roi", "seg_bin", "seg"]:
+                        segmentation_root = segmentation_sitk.TransformIndexToPhysicalPoint((0, 0, 0))
+                        segmentation_root = data_sitk.TransformPhysicalPointToIndex(segmentation_root)
+                        segmentation_root = np.array(segmentation_root) - np.array(self.roi_root)
+                        assert all(x >= 0 for x in segmentation_root)
+                        segmentation_size = segmentation_sitk.GetSize()
+                        assert all(seg_root + seg_size <= roi_size for seg_root, seg_size, roi_size in
+                                   zip(segmentation_root, segmentation_size, self.roi_size))
+                        if self.crop == "roi":
+                            mask = np.zeros_like(data_np, dtype=np.bool)
+                            mask[segmentation_root[0]:segmentation_root[0] + segmentation_size[0],
+                                 segmentation_root[1]:segmentation_root[1] + segmentation_size[1],
+                                 segmentation_root[2]:segmentation_root[2] + segmentation_size[2]] = True
+                            import matplotlib.pyplot as plt
+                            for img in np.split(mask, indices_or_sections=mask.shape[2], axis=2):
+                                img = np.squeeze(img)
+                                plt.imshow(img)
+                                plt.show()
+                            data_np = np.where(mask, data_np, 0)
+                        else:
+                            segmentation_np = sitk.GetArrayFromImage(segmentation_sitk)
+                            # TODO crop to segmentation
+                        data_np = np.expand_dims(data_np, axis=0)
+
+                batch_np.append(data_np)
 
             batch = {"data": np.stack(batch_np)}
 
