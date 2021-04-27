e545f4ab6e0be6c779ce73d854a79b69a2489cfe
Martin Lelis <martinlelis@yahoo.de>
2021-04-27 19:21:09 +0200

    add simple 3d cnn

---
+++ models/simple_net.py
@@ -0,0 +1,30 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# based on https://arxiv.org/abs/2007.13224
+
+
+def get_model(input_shape):
+    model = keras.Sequential(name="SimpleNet")
+    model.add(layers.Conv3D(filters=64, kernel_size=3, activation="relu", input_shape=input_shape))
+    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv3D(filters=64, kernel_size=3, activation="relu", input_shape=input_shape))
+    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv3D(filters=128, kernel_size=3, activation="relu", input_shape=input_shape))
+    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv3D(filters=256, kernel_size=3, activation="relu", input_shape=input_shape))
+    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.GlobalAveragePooling3D())
+    model.add(layers.Dense(units=512, activation="relu"))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Dense(units=1, activation="sigmoid"))
+    return model
