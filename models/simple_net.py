6569cf7fdcbfea371aec17a2aabe61fa5a2d6e4c
Martin Lelis <martinlelis@yahoo.de>
2021-04-29 13:41:39 +0200

    fix pool size of max pool layer in simple net

--- models/simple_net.py
+++ models/simple_net.py
@@ -7,19 +7,19 @@ from tensorflow.keras import layers
 def get_model(input_shape=(None, None, None, 1)):
     model = keras.Sequential(name="SimpleNet")
     model.add(layers.Conv3D(filters=64, kernel_size=3, padding="same", activation="relu", input_shape=input_shape))
-    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.MaxPool3D())
     model.add(layers.BatchNormalization())
 
     model.add(layers.Conv3D(filters=64, kernel_size=3, padding="same", activation="relu"))
-    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.MaxPool3D())
     model.add(layers.BatchNormalization())
 
     model.add(layers.Conv3D(filters=128, kernel_size=3, padding="same", activation="relu"))
-    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.MaxPool3D())
     model.add(layers.BatchNormalization())
 
     model.add(layers.Conv3D(filters=256, kernel_size=3, padding="same", activation="relu"))
-    model.add(layers.MaxPool3D(pool_size=2))
+    model.add(layers.MaxPool3D())
     model.add(layers.BatchNormalization())
 
     model.add(layers.GlobalAveragePooling3D())
