89a166152ab2aa7c412eeefc82562e9cccf23db7
Martin Lelis <martinlelis@yahoo.de>
2021-05-03 19:40:51 +0200

    add early stopping callback

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from argparse import ArgumentParser
 
@@ -35,13 +36,18 @@ def main():
     # get input shape from dummy loader
     dummy_loader = MultiThreadedAugmenter(DataLoader(data=full_dataset[:1], batch_size=1), PrepareForTF(), 1)
     input_shape = next(dummy_loader)[0].shape[-4:]
-    #input_shape = [*[int(2**np.ceil(np.log2(x))) for x in input_shape[:-1]], 1]
-    #transforms.append(ResizeTransform(input_shape[:-1]))
+    # input_shape = [*[int(2**np.ceil(np.log2(x))) for x in input_shape[:-1]], 1]
+    # transforms.append(ResizeTransform(input_shape[:-1]))
     transforms = [MirrorTransform(),
                   RandomShiftTransform(shift_mu=0, shift_sigma=3, p_per_channel=1),
                   PrepareForTF()]
     transforms = Compose(transforms)
     for i, (train, validation) in enumerate(k_fold.split(full_dataset)):
+        checkpoint_dir = os.path.join(config["workspace"], "cross_validation", str(i))
+        log_dir = os.path.join(checkpoint_dir, "logs")
+        history_file = os.path.join(log_dir, "history.json")
+        if os.path.exists(history_file):
+            continue
         dl_train = MultiThreadedAugmenter(DataLoader(data=[full_dataset[x] for x in train],
                                                      batch_size=config_data["batch_size"],
                                                      number_of_threads_in_multithreaded=config_data["loader_threads"]),
@@ -57,23 +63,26 @@ def main():
         model.summary()
         model.compile(optimizer=config_training["optimizer"],
                       loss=tf.losses.BinaryCrossentropy(from_logits=True),
-                      metrics=[tf.metrics.AUC])
-        checkpoint_dir = os.path.join(config["workspace"], "cross_validation", str(i))
+                      metrics=["auc"])
         checkpoint_file = os.path.join(checkpoint_dir, "{epoch:04d}.ckpt")
-        save_frequency = int((len(train) / config_data["batch_size"]) * config_training["save_frequency"])
         checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_file,
                                                                  save_weights_only=True,
-                                                                 save_freq=save_frequency)
+                                                                 save_best_only=True)
+        early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor="auc",
+                                                                   patience=config_training["es_patience"])
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, write_graph=False)
         latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
         initial_epoch = 0
         if latest_checkpoint is not None:
             model.load_weights(latest_checkpoint)
             initial_epoch = int(os.path.splitext(os.path.basename(latest_checkpoint))[0])
-        model.fit(x=dl_train,
-                  initial_epoch=initial_epoch,
-                  epochs=config_training["epochs"],
-                  validation_data=dl_validation,
-                  callbacks=[checkpoint_callback])
+        history = model.fit(x=dl_train,
+                            initial_epoch=initial_epoch,
+                            epochs=config_training["epochs"],
+                            validation_data=dl_validation,
+                            callbacks=[checkpoint_callback, early_stopping_callback, tensorboard_callback])
+        with open(history_file, "w") as file:
+            json.dump(history.history, file)
 
 
 if __name__ == '__main__':
