f5e1d98f0e76ddd787f408526ba9b1b08b0f5f44
Martin Lelis <martinlelis@yahoo.de>
2021-05-04 14:38:27 +0200

    balance CV split, fix early stopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 from argparse import ArgumentParser
 
 import tensorflow as tf
@@ -22,6 +23,14 @@ def load_model(model_string, input_shape):
     return model_mapping[model_string](input_shape)
 
 
+# augmentation
+def get_transforms():
+    transforms = [MirrorTransform(),
+                  RandomShiftTransform(shift_mu=0, shift_sigma=3, p_per_channel=1),
+                  PrepareForTF()]
+    return Compose(transforms)
+
+
 def main():
     arg_parser = ArgumentParser()
     arg_parser.add_argument("--config", "-c", default=os.path.join(script_dir, "default.config"))
@@ -32,43 +41,46 @@ def main():
     config_data = config["data"]
 
     full_dataset = scan_data_directory(config_data["path"], crop=config_data["crop"])
+    dataset_true = [x for x in full_dataset if x[0] == 1]
+    dataset_false = [x for x in full_dataset if x[0] == 0]
     k_fold = KFold(n_splits=config_training["folds"], shuffle=False)
     # get input shape from dummy loader
     dummy_loader = MultiThreadedAugmenter(DataLoader(data=full_dataset[:1], batch_size=1), PrepareForTF(), 1)
     input_shape = next(dummy_loader)[0].shape[-4:]
     # input_shape = [*[int(2**np.ceil(np.log2(x))) for x in input_shape[:-1]], 1]
     # transforms.append(ResizeTransform(input_shape[:-1]))
-    transforms = [MirrorTransform(),
-                  RandomShiftTransform(shift_mu=0, shift_sigma=3, p_per_channel=1),
-                  PrepareForTF()]
-    transforms = Compose(transforms)
-    for i, (train, validation) in enumerate(k_fold.split(full_dataset)):
+    transforms = get_transforms()
+    for i, ((train_t, validation_t), (train_f, validation_f)) \
+            in enumerate(zip(k_fold.split(dataset_true), k_fold.split(dataset_false))):
         checkpoint_dir = os.path.join(config["workspace"], "cross_validation", str(i))
         log_dir = os.path.join(checkpoint_dir, "logs")
         history_file = os.path.join(log_dir, "history.json")
         if os.path.exists(history_file):
             continue
-        dl_train = MultiThreadedAugmenter(DataLoader(data=[full_dataset[x] for x in train],
+        train = [dataset_true[x] for x in train_t] + [dataset_false[x] for x in train_f]
+        random.shuffle(train)
+        validation = [dataset_true[x] for x in validation_t] + [dataset_false[x] for x in validation_f]
+        random.shuffle(validation)
+        dl_train = MultiThreadedAugmenter(DataLoader(data=train,
                                                      batch_size=config_data["batch_size"],
                                                      number_of_threads_in_multithreaded=config_data["loader_threads"]),
                                           transforms,
                                           config_data["loader_threads"])
-        dl_validation = MultiThreadedAugmenter(DataLoader(data=[full_dataset[x] for x in validation],
+        dl_validation = MultiThreadedAugmenter(DataLoader(data=validation,
                                                           batch_size=config_data["batch_size"],
                                                           number_of_threads_in_multithreaded=config_data[
                                                               "loader_threads"]),
                                                transforms,
                                                config_data["loader_threads"])
         model = load_model(config["model"], input_shape)
-        model.summary()
         model.compile(optimizer=config_training["optimizer"],
                       loss=tf.losses.BinaryCrossentropy(from_logits=True),
-                      metrics=["auc"])
+                      metrics=["AUC"])
         checkpoint_file = os.path.join(checkpoint_dir, "{epoch:04d}.ckpt")
         checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_file,
                                                                  save_weights_only=True,
                                                                  save_best_only=True)
-        early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor="auc",
+        early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor="val_AUC",
                                                                    patience=config_training["es_patience"])
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, write_graph=False)
         latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
