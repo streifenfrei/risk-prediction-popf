09942d8e9fe0957fb4d04206930dabc2e6e4deef
Martin Lelis <martinlelis@yahoo.de>
2021-06-17 13:24:42 +0200

    add regularization to ResNet

--- models/res_net.py
+++ models/res_net.py
@@ -3,13 +3,15 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
-def residual_block(x, filters, strides=1):
-    skip = layers.Conv3D(filters=filters, kernel_size=3, strides=strides, activation="relu", padding="same")(x)
+def residual_block(x, filters, strides=1, regularizer=None):
+    skip = layers.Conv3D(filters=filters, kernel_size=3, strides=strides, activation="relu", padding="same",
+                         kernel_regularizer=regularizer)(x)
     skip = layers.BatchNormalization()(skip)
 
-    x = layers.Conv3D(filters=filters, kernel_size=3, strides=strides, activation="relu", padding="same")(x)
+    x = layers.Conv3D(filters=filters, kernel_size=3, strides=strides, activation="relu", padding="same",
+                      kernel_regularizer=regularizer)(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Conv3D(filters=filters, kernel_size=3, padding="same")(x)
+    x = layers.Conv3D(filters=filters, kernel_size=3, padding="same", kernel_regularizer=regularizer)(x)
     x = layers.BatchNormalization()(x)
 
     x = tf.add(skip, x)
@@ -18,22 +20,27 @@ def residual_block(x, filters, strides=1):
     return x
 
 
-def get_model(ct_shape=(None, None, None, 1), vector_shape=None, first_conv_channel=16, dropout=0.3):
+def get_model(ct_shape=(None, None, None, 1),
+              vector_shape=None,
+              first_conv_channel=16,
+              dropout=0.3,
+              regularizer=None):
     ct = layers.Input(shape=ct_shape)
     inputs = [ct]
-    x = layers.Conv3D(filters=first_conv_channel, kernel_size=3, strides=2, activation="relu", input_shape=ct_shape)(ct)
+    x = layers.Conv3D(filters=first_conv_channel, kernel_size=3, strides=2, activation="relu",
+                      input_shape=ct_shape, kernel_regularizer=regularizer)(ct)
     x = layers.BatchNormalization()(x)
 
-    x = residual_block(x, first_conv_channel)
-    x = residual_block(x, first_conv_channel)
-    x = residual_block(x, first_conv_channel, strides=2)
-    x = residual_block(x, first_conv_channel)
-    x = residual_block(x, 2*first_conv_channel, strides=2)
-    x = residual_block(x, 2*first_conv_channel)
-    x = residual_block(x, 4*first_conv_channel)
-    x = residual_block(x, 4*first_conv_channel)
+    x = residual_block(x, first_conv_channel, regularizer=regularizer)
+    x = residual_block(x, first_conv_channel, regularizer=regularizer)
+    x = residual_block(x, first_conv_channel, strides=2, regularizer=regularizer)
+    x = residual_block(x, first_conv_channel, regularizer=regularizer)
+    x = residual_block(x, 2*first_conv_channel, strides=2, regularizer=regularizer)
+    x = residual_block(x, 2*first_conv_channel, regularizer=regularizer)
+    x = residual_block(x, 4*first_conv_channel, regularizer=regularizer)
+    x = residual_block(x, 4*first_conv_channel, regularizer=regularizer)
 
     x = layers.GlobalAveragePooling3D()(x)
     x = layers.Dropout(rate=dropout)(x)
-    x = layers.Dense(units=1, activation="sigmoid")(x)
+    x = layers.Dense(units=1, activation="sigmoid", kernel_regularizer=regularizer)(x)
     return tf.keras.Model(inputs=inputs, outputs=x, name="ResNet")
